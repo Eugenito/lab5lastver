f3fd0e524b4673edafe690caa45378fd0dfe7cde
Eugenito <72038188+Eugenito@users.noreply.github.com>
2020-11-25 13:03:36 +0300

    Add files via upload

---
+++ lab5lastver1.cpp
@@ -0,0 +1,48 @@
+﻿#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+	int m;
+	int array[3][3];
+	system("chcp 1251");
+	system("cls");
+	printf("Введите элементы массива\n");
+	for (int i = 0;i < 3;i++)
+	{
+		for (int j = 0;j < 3;j++)
+		{
+			scanf_s("%d", &array[i][j]);
+		}
+	}
+	for (int i = 0;i < 3;i++)
+	{
+
+		for (int z = 0;z < 2;z++)
+		{
+			for (int j = 1;j < 3;j++)
+			{
+
+
+				if (array[i][z] < array[i][j])
+				{
+					m = array[i][z];
+					array[i][z] = array[i][j];
+					array[i][j] = m;
+				}
+
+			}
+		}
+
+
+	}
+	for (int i = 0;i < 3;i++)
+	{
+		printf("\n");
+		for (int j = 0;j < 3;j++)
+		{
+			printf("%d", array[i][j]);
+		}
+	}
+	return 0;
+}
